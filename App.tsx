2a46c7c78ab1db84312095b55270fe6af9df997e
lucaspelloni2 <lucas_pelloni@hotmail.com>
2019-09-27 23:14:17 +0200

    typescript

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React, {ReactNode} from 'react';
+import {StatusBar} from 'react-native';
+
+const App: () => ReactNode = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+    </>
+  );
+};
+
+export default App;
