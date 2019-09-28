875c8d8942cbe9999c0c5033aa0e784c2a7db8d5
lucaspelloni2 <lucas_pelloni@hotmail.com>
2019-09-28 02:17:22 +0200

    oke

--- App.tsx
+++ App.tsx
@@ -1,6 +1,15 @@
 import React, {ReactNode} from 'react';
-import {StatusBar} from 'react-native';
-import AppNavigator from "./src/navigation/AppNavigator";
+import {StatusBar, YellowBox} from 'react-native';
+import AppNavigator from './src/navigation/AppNavigator';
+
+YellowBox.ignoreWarnings([
+  'Remote debugger',
+  'Warning: componentWillMount is deprecated',
+  'Warning: componentWillMount has been renamed',
+  'Warning: componentWillReceiveProps is deprecated',
+  'Warning: componentWillReceiveProps has been renamed',
+  '-[RCTRootView cancelTouches]',
+]);
 
 const App: () => ReactNode = () => {
   return (
--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -8,67 +8,128 @@ import {
   Wrapper,
 } from '../layout/Layout';
 import {NavigationScreenProp} from 'react-navigation';
-import {Image} from 'react-native';
+import {Image, StyleSheet} from 'react-native';
 import Menu from '../assets/icons/menu.svg';
 import {__COLORS} from '../layout/Colors';
 import {getAlphaColor} from '../layout/AlphaColor';
 import {InputField} from '../components/InputField';
+import Animated, {Easing} from 'react-native-reanimated';
+import {__FONT_FAMILIES} from '../layout/Fonts';
+import {useTransition} from 'react-native-redash';
 
 const Container = styled(MyView)`
   flex: 1;
   margin-top: ${SPACING * 4}px;
 `;
 
-const Title = styled(TextExtraBold)`
-  text-align: center;
-`;
 const Hack = styled(MyView)`
   position: absolute;
   top: ${SPACING * 7}px;
-  left: ${SPACING * 3}px;
+  left: ${SPACING * 3.5}px;
 `;
 
-const SubTitle = styled(TextRegular)`
-  color: ${getAlphaColor(0.5, __COLORS.WHITE)};
-  text-align: center;
-  margin-top: -${SPACING / 2}px;
-`;
-const Header = styled(MyView)`
-  padding-top: ${SPACING * 16}px;
-`;
+
 type Props = {
   navigation: NavigationScreenProp<any, any>;
 };
 
 type State = {
   searchValue: string;
+  isOnFocus: boolean;
 };
 
+const titleStyle = StyleSheet.create({
+  title: {
+    fontSize: 40,
+    textAlign: 'center',
+    color: __COLORS.TEXT_COLOR,
+    fontFamily: __FONT_FAMILIES.BOLD,
+  },
+  subTitle: {
+    fontSize: 20,
+    color: getAlphaColor(0.5, __COLORS.WHITE),
+    textAlign: 'center',
+    marginTop: -SPACING / 2,
+    fontFamily: __FONT_FAMILIES.REGULAR,
+  },
+});
+
+type TitleProps = {
+  isOnFocus: boolean;
+};
+
+const AnimateSalt = ({isOnFocus}: TitleProps) => {
+  return (
+    <Hack>
+      <Menu width={30} height={30} />
+    </Hack>
+  );
+};
+
+const AnimatedTitle = ({isOnFocus}: TitleProps) => {
+  let {not, interpolate} = Animated;
+  const animatedValue = isOnFocus ? 1 : 0;
+  const transition = useTransition(
+    animatedValue,
+    not(animatedValue),
+    animatedValue,
+    400,
+    Easing.ease,
+  );
+
+  const inputRange = [0, 1];
+  let fontSize = interpolate(transition, {
+    inputRange,
+    outputRange: [40, 20],
+  });
+  let paddingTop = interpolate(transition, {
+    inputRange,
+    outputRange: [SPACING * 15, 0],
+  });
+  let opacity = interpolate(transition, {
+    inputRange,
+    outputRange: [1, 0],
+  });
+  return (
+    <>
+      <Animated.Text
+        style={[titleStyle.title, {fontSize}, {paddingTop}, {opacity}]}>
+        This guil Recipe
+      </Animated.Text>
+      <Animated.Text style={[titleStyle.subTitle, {opacity}]}>
+        This guil Recipe
+      </Animated.Text>
+    </>
+  );
+};
 class MainScreen extends React.Component<Props, State> {
   state = {
     searchValue: '',
+    isOnFocus: false,
   };
   componentDidMount(): void {}
 
   render() {
     const {navigation} = this.props;
     const {searchValue} = this.state;
+    console.log(this.state.isOnFocus);
     return (
       <>
         <Image
           style={{position: 'absolute'}}
           source={require('../assets/images/background.png')}
         />
-        <Hack>
-          <Menu width={40} height={40} />
-        </Hack>
+        <AnimateSalt isOnFocus={this.state.isOnFocus} />
         <Wrapper number={1}>
-          <Header>
-            <Title fontSize={35}>This guil Recipe</Title>
-            <SubTitle fontSize={20}>This guil Recipe</SubTitle>
-          </Header>
+          <AnimatedTitle isOnFocus={this.state.isOnFocus} />
           <Container>
             <InputField
+              onChangeBlur={() => {
+                this.setState({isOnFocus: false});
+              }}
+              onChangeFocus={() => {
+                this.setState({isOnFocus: true});
+              }}
               onTextChange={(searchValue: string) => {
                 this.setState({searchValue});
               }}
