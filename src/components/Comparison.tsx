59fe4e6f1808d2cc62e30d72ead36ad2cdf54607
lucaspelloni2 <lucas_pelloni@hotmail.com>
2019-09-28 23:13:00 +0200

    I think it complies

--- src/components/Comparison.tsx
+++ src/components/Comparison.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from 'styled-components';
 import Card from './Card';
-import {Image} from 'react-native';
-import {MyView, TextBold} from '../layout/Layout';
-import {RecipeEvaluationPossibility} from '../helpers/backend-types';
+import {Image, StyleSheet, View} from 'react-native';
+import {
+  __WINDOW_HEIGHT,
+  __WINDOW_WIDTH,
+  MyView,
+  SPACING,
+} from '../layout/Layout';
+import {Recipe, RecipeEvaluationPossibility} from '../helpers/backend-types';
+import {__COLORS} from '../layout/Colors';
+import Hack from './Hack';
 
 type Props = {
   possibilities: RecipeEvaluationPossibility[];
+  recipe: Recipe;
 };
 
 const MyCard = styled(MyView)<{color: string}>`
@@ -18,12 +26,126 @@ const MyCard = styled(MyView)<{color: string}>`
 
 const Container = styled(MyView)`
   flex: 1;
+  background: #1e201b;
 `;
 
-export const Comparison = ({possibilities}: Props) => {
+const Coop = styled(MyView)``;
+
+const Migros = styled(MyView)`
+  flex: 1;
+  justify-content: flex-end;
+  align-items: flex-end;
+  width: ${__WINDOW_WIDTH}px;
+  padding-bottom: 30px;
+  padding-right: 20px;
+`;
+
+const Absolute = styled(MyView)`
+  position: absolute;
+  top: 0;
+  height: 100%;
+`;
+
+const SuperHack = styled(MyView)`
+  position: absolute;
+  right: -10px;
+  bottom: 200px;
+`;
+
+const SuperHack2 = styled(MyView)`
+  position: absolute;
+  left: -150px;
+  top: 120px;
+`;
+export const Comparison = ({possibilities, recipe}: Props) => {
+  const [migrosScore, setMigrosScore] = useState(0);
+  const [coopScore, setCoopScore] = useState(0);
+
+  const [migrosScores, setMigrosScores] = useState([]);
+  const [coopScores, setCoopScores] = useState([]);
+  // @ts-ignore
+  const grams = recipe.ingredients.map(i => i.gram);
+
+  useEffect(() => {
+    possibilities.map(p => {
+      let offsets = p.ingredients
+        .filter(i => i.products.length > 0)
+        .map(i => i.products[0].co2Offset);
+      let totalGrams = 0;
+      let totalOffset = 0;
+      for (let i = 0; i < grams.length; i++) {
+        if (!grams[i] || !offsets[i]) {
+          continue;
+        }
+        const gram = grams[i];
+        totalGrams += gram;
+        const offset = offsets[i];
+        totalOffset += offset;
+      }
+      const co2Score = (totalOffset / totalGrams) * 100;
+      if (p.store === 'COOP') {
+        setCoopScore(co2Score);
+        setCoopScores(offsets);
+        console.log(offsets);
+      } else if (p.store === 'MIGROS') {
+        setMigrosScore(co2Score);
+        setMigrosScores(offsets);
+      }
+    });
+  }, []);
+
+  console.log('scoree', migrosScore, coopScore);
+
   return (
     <Card>
-
+      <Container>
+        <View style={[styles.trangleCorner]} />
+        <Absolute>
+          <Coop>
+            <Image
+              source={require('../assets/images/coop.png')}
+              style={{width: 100, height: 100, marginLeft: 10}}
+              resizeMode={'contain'}
+            />
+            <SuperHack2>
+              <Hack
+                co2Score={coopScore}
+                recipe={recipe}
+                scores={setCoopScores}
+              />
+            </SuperHack2>
+          </Coop>
+        </Absolute>
+        <Absolute>
+          <Migros>
+            <Image
+              source={require('../assets/images/migros.png')}
+              style={{width: 120, height: 100, marginLeft: 10}}
+              resizeMode={'contain'}
+            />
+            <SuperHack>
+              <Hack
+                co2Score={migrosScore}
+                recipe={recipe}
+                scores={migrosScores}
+              />
+            </SuperHack>
+          </Migros>
+        </Absolute>
+      </Container>
     </Card>
   );
 };
+
+const styles = StyleSheet.create({
+  trangleCorner: {
+    width: 0,
+    height: 0,
+    backgroundColor: 'transparent',
+    borderStyle: 'solid',
+    borderRightWidth: __WINDOW_WIDTH,
+    borderTopWidth: __WINDOW_HEIGHT - SPACING * 11,
+    borderRightColor: 'transparent',
+    borderTopColor: 'white',
+  },
+});
---
+++ src/components/Hack.tsx
@@ -0,0 +1,145 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import {PieChart} from 'react-native-svg-charts';
+import {__COLORS, __GRAY_COLORS} from '../layout/Colors';
+import getCo2Hue from '../helpers/get-co2-hue';
+import {__WINDOW_WIDTH, MyView, TextLight, TextRegular} from '../layout/Layout';
+import {StyleSheet, View, Image, Text} from 'react-native';
+import {getAlphaColor} from '../layout/AlphaColor';
+import {Recipe} from '../helpers/backend-types';
+
+type Props = {
+  recipe: Recipe;
+  scores: number[];
+  co2Score: number;
+  currentScore: number;
+  currentIndex: number;
+};
+
+const Overlay = styled(MyView)``;
+
+const Container = styled(MyView)`
+  position: absolute;
+  width: ${__WINDOW_WIDTH / 2}px;
+  height: 170px;
+`;
+
+const Shadow = StyleSheet.create({
+  shadow: {
+    shadowColor: 'rgba(0,0,0, .2)',
+    shadowOffset: {height: 0, width: 0},
+    shadowOpacity: 10,
+    shadowRadius: 5,
+  },
+});
+
+class Hack extends React.PureComponent<Props, {}> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedSlice: {
+        label: '',
+        value: 0,
+      },
+      labelWidth: 0,
+    };
+  }
+
+  render() {
+    // @ts-ignore
+    const {labelWidth, selectedSlice} = this.state;
+    const {label, value} = selectedSlice;
+
+    // @ts-ignore
+    let scores = [...new Set(this.props.scores)];
+
+    console.log('All scores, ', scores);
+    console.log('current score', this.props.currentScore);
+
+    if (scores.length === 0) {
+      scores.push(50);
+      scores.push(100);
+    }
+    console.log('current index ', this.props.currentIndex);
+    const colors = scores.map(s => {
+      if (s === this.props.currentScore || this.props.currentIndex === 0) {
+        return getAlphaColor(0.85, getCo2Hue(s));
+      } else {
+        return getAlphaColor(0.5, getCo2Hue(s));
+      }
+    });
+
+    let ratio =
+      scores.filter(x => x > 200).length === 0
+        ? 4
+        : scores.filter(x => x > 500).length === 0
+        ? 7
+        : 10;
+
+    const data = scores.map((key, index) => {
+      return {
+        key,
+        value: scores[index],
+        svg: {fill: colors[index]},
+        arc: {
+          outerRadius: 70 + scores[index] / ratio + '%',
+          padAngle: 0.01,
+        },
+        onPress: () =>
+          this.setState({selectedSlice: {label: key, value: scores[index]}}),
+      };
+    });
+    let ingriedients = this.props.recipe.ingredients;
+    ingriedients.map(i => {
+      let food = i.food;
+    });
+
+    // wenn 2 in co2offset -> 200g CO2 pro 100g Food
+    // food: in gram
+    return (
+      <View
+        style={[
+          {
+            paddingTop: 20,
+            width: __WINDOW_WIDTH,
+            flex: 1,
+          },
+          Shadow.shadow,
+        ]}>
+        <PieChart
+          style={{height: 150, marginLeft: 175, paddingBottom: 15}}
+          outerRadius={'70%'}
+          innerRadius={'50%'}
+          data={data}
+        />
+
+        <TextRegular
+          fontSize={20}
+          onLayout={({
+            nativeEvent: {
+              layout: {width},
+            },
+          }) => {
+            this.setState({labelWidth: width});
+          }}
+          style={[
+            {
+              top: 68,
+              color: __GRAY_COLORS._BLACK,
+              position: 'absolute',
+              right: 82,
+              textAlign: 'center',
+            },
+          ]}>
+          {Math.round(this.props.co2Score).toFixed(0)}
+        </TextRegular>
+        <Text style={{fontSize: 10, position: 'absolute', right: 72, top: 95}}>
+          g CO₂/100g
+        </Text>
+      </View>
+    );
+  }
+}
+
+export default Hack;
